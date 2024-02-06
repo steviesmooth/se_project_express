00519a903d3ab10de40246297dbfa0bb612be7a5
steviesmooth <naraksteve@yahoo.com>
2024-02-05 21:34:35 -0600

    guyuygvtfrtf

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 const { PORT = 3001 } = process.env;
 const mongoose = require("mongoose");
 
+app.listen(PORT, () => {
+  console.log("this is working");
+});
+
 const routes = require("./routes");
 
 app.use(express.json());
@@ -17,8 +21,4 @@ app.use((req, res, next) => {
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
   console.log("connected to DB", r);
-});
-
-app.listen(PORT, () => {
-  console.log("this is working");
 });
