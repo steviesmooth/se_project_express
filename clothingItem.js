0957b67daf212ee27a10c7d4aa39470269b4d967
Stephen Narak <126042891+steviesmooth@users.noreply.github.com>
2024-02-05 19:12:13 -0600

    Add files via upload

--- clothingItem.js
+++ clothingItem.js
@@ -1,37 +1,97 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const ClothingItem = require("../se_project_express/models/clothingItem");
+const {
+  Bad_Request_Error,
+  Not_Found_Error,
+  Server_Error,
+} = require("../se_project_express/utils/errors");
 
-const clothingItem = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  weather: {
-    type: String,
-    required: true,
-    enum: ["hot", "warm", "cold"],
-  },
-  imageURL: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => validator.isURL(v),
-      message: "Link is not Valid",
-    },
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  console.log(req.user._id);
+  const userId = req.user._id;
+  const { name, weather, imageURL } = req.body;
 
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  ClothingItem.create({ name, weather, imageURL, owner: userId })
+    .then((item) => {
+      console.log(item);
+      res.status(201).send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        res.status(Bad_Request_Error).send({ message: err.message });
+      } else {
+        res.status(Server_Error).send({ message: err.message });
+      }
+    });
+};
 
-module.exports = mongoose.model("clothingItems", clothingItem);
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      res.status(Server_Error).send({ message: "Error from getItems", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(201).send({ data: item }))
+    .catch((e) => {
+      res.status(Server_Error).send({ message: "Error from updateItem", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res
+        .status(Bad_Request_Error)
+        .send({ message: "Error from deleteItem", e });
+    });
+};
+
+//like item
+
+const likeItem = (req, res) =>
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(201).send({ data: item }))
+    .catch((e) => {
+      res.status(Bad_Request_Error).send({ message: "Error from likeItem", e });
+    });
+
+const dislikeItem = (req, res) =>
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } }, // remove _id from the array
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.status(201).send({ data: item }))
+    .catch((e) => {
+      res
+        .status(Not_Found_Error)
+        .send({ message: "Error from dislikeItem", e });
+    });
+
+module.exports = {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+  likeItem,
+  dislikeItem,
+};
---
+++ users.js
@@ -0,0 +1,51 @@
+const User = require("../se_project_express/models/user");
+const {
+  Bad_Request_Error,
+  Not_Found_Error,
+  Server_Error,
+} = require("../se_project_express/utils/errors");
+
+// get users
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(Server_Error).send({ message: err.message });
+    });
+};
+
+// create user
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        res.status(Bad_Request_Error).send({ message: err.message });
+      } else {
+        res.status(Server_Error).send({ message: err.message });
+      }
+    });
+};
+
+const getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(() => {
+      const error = new Error("User Id not found");
+      err.statusCode = 404;
+      throw error;
+    })
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = { getUsers, createUser, getUser };
