c69e4b8ebcf4ef1c5dff91274926d53c926e38a1
Stephen Narak <126042891+steviesmooth@users.noreply.github.com>
2024-02-05 19:10:39 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { PORT = 3001 } = process.env;
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "65c11aac4ee01d83701127ff", // paste the _id of the test user created in the previous step
+  };
+  next();
+});
+app.use(routes);
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) => {
+  console.log("connected to DV", r);
+});
+
+app.listen(PORT, () => {
+  console.log("this is working");
+});
