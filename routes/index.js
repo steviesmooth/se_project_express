4ee4b4916f449b0ae52aa78d6c3a3741f1f26e76
steviesmooth <naraksteve@yahoo.com>
2024-02-06 16:47:41 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const clothingItem = require("../controllers/clothingItem");
-const users = require("../controllers/users");
+const clothingItem = require("../routes/clothingItem");
+const users = require("../routes/users");
 
-router.use("/items", () => clothingItem);
-router.use("/users", () => users);
+router.use("/items", clothingItem);
+router.use("/users", users);
 
 router.use((req, res) => {
   res.status(500).send({ message: "Router not found" });
