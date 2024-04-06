4b654d050b9a371eb3adcdb388a832ca61bfe9b3
steviesmooth <naraksteve@yahoo.com>
2024-04-06 12:57:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(cors());
 const routes = require("./routes/index");
 
 app.use(express.json());
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.post("/signin", login);
 app.post("/signup", createUser);
 
