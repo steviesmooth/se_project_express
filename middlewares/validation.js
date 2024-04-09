3b10c497852f08b7303df160368702118cf57666
steviesmooth <naraksteve@yahoo.com>
2024-04-08 22:42:27 -0500

    Update validation.js

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -20,6 +20,9 @@ module.exports.validateCardBody = celebrate({
       "string.empty": 'The "imageUrl" field must be filled in',
       "string.uri": 'the "imageUrl" field must be a valid url',
     }),
+    weather: Joi.string().valid("hot", "warm", "cold").required().messages({
+      "sting.empty": "Field is required",
+    }),
   }),
 });
 module.exports.validateUserUpdate = celebrate({
